924ec73263aabf44dacaebd6766ac7ca5ce94876
coprum96 <118367805+coprum96@users.noreply.github.com>
2023-04-16 13:06:05 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ client/src/pages/DevicePage.js
@@ -0,0 +1,70 @@
+import React, {useEffect, useState, useContext} from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/bigStar.png'
+import {useParams} from 'react-router-dom'
+import {fetchOneDevice} from "../http/deviceAPI";
+import {Context} from "../index";
+import { BASKET_ROUTE } from '../utils/consts';
+import {useNavigate} from 'react-router-dom';
+
+const DevicePage = () => {
+    const [device, setDevice] = useState({ info: [] });
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const { device: deviceStore } = useContext(Context);
+
+    useEffect(() => {
+        fetchOneDevice(id).then((data) => setDevice(data));
+    }, [id]);
+
+    const handleBuy = () => {
+        const basketDevice = {
+            id: device.id,
+            name: device.name,
+            price: device.price,
+        };
+        deviceStore.addToBasket(basketDevice);
+        navigate(BASKET_ROUTE, { state: { purchasedDeviceId: device.id } });
+    };
+
+    return (
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={process.env.REACT_APP_API_URL + device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{background: `url(${bigStar}) no-repeat center center`, width:240, height: 240, backgroundSize: 'cover', fontSize:64}}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        style={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>₪{device.price}</h3>
+                        <Button variant={"outline-dark"} onClick={handleBuy}>Buy now</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column m-3">
+                <h1>Characteristics</h1>
+                {device.info.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title} {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
+    );
+};
+
+export default DevicePage;
+
